a0b81a1837eb933a8939e007f1f44881e77462b9
bigpel66 <bigpel66@icloud.com>
2024-04-22 14:52:24 +0900

    fix: WebMvcConfig 설정 변경

--- src/main/java/camp/nextstep/config/WebMvcConfig.java
+++ src/main/java/camp/nextstep/config/WebMvcConfig.java
@@ -1,5 +1,6 @@
 package camp.nextstep.config;
 
+import camp.nextstep.support.ResourceVersion;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.context.annotation.Bean;
@@ -12,16 +13,13 @@ import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import org.springframework.web.servlet.mvc.WebContentInterceptor;
-import camp.nextstep.support.ResourceVersion;
 
-import javax.servlet.Filter;
 import java.time.Duration;
 
 @Configuration
 @EnableWebMvc
 public class WebMvcConfig implements WebMvcConfigurer {
     public static final String PREFIX_STATIC_RESOURCES = "/resources";
-
 
     private final ResourceVersion version;
 
@@ -32,23 +30,24 @@ public class WebMvcConfig implements WebMvcConfigurer {
 
     @Override
     public void addInterceptors(final InterceptorRegistry registry) {
+        String allPath = "/**";
+        String resourceVersionPath = PREFIX_STATIC_RESOURCES + "/" + version.getVersion() + "/**";
         WebContentInterceptor interceptor = new WebContentInterceptor();
-        //no-store, no-cache, must-revalidate 를 확인해보려면 인터셉터 구간 설정을 뺴고 테스트해보시면 됩니다.
-        interceptor.addCacheMapping(CacheControl.noCache().cachePrivate(), "/**");
+        interceptor.addCacheMapping(CacheControl.noCache().cachePrivate(), allPath);
+        interceptor.addCacheMapping(CacheControl.maxAge(Duration.ofDays(365)), resourceVersionPath);
         registry.addInterceptor(interceptor);
     }
 
     @Bean
-    public FilterRegistrationBean filterRegistrationBean(){
-        FilterRegistrationBean registration = new FilterRegistrationBean();
-        Filter etagHeaderFilter = new ShallowEtagHeaderFilter();
-        registration.setFilter(etagHeaderFilter);
-        registration.addUrlPatterns("/etag", PREFIX_STATIC_RESOURCES + "/*");
+    public FilterRegistrationBean<ShallowEtagHeaderFilter> filterRegistrationBean() {
+        FilterRegistrationBean<ShallowEtagHeaderFilter> registration = new FilterRegistrationBean<>();
+        registration.setFilter(new ShallowEtagHeaderFilter());
+        registration.addUrlPatterns("/etag");
         return registration;
     }
 
     @Bean
-    public FilterRegistrationBean<HttpHeaderFilter> httpHeaderFilter(){
+    public FilterRegistrationBean<HttpHeaderFilter> httpHeaderFilter() {
         var filter = new HttpHeaderFilter();
         final FilterRegistrationBean<HttpHeaderFilter> registrationBean = new FilterRegistrationBean<>();
         registrationBean.setFilter(filter);
@@ -57,6 +56,7 @@ public class WebMvcConfig implements WebMvcConfigurer {
     }
 
     @Override
+
     public void addResourceHandlers(final ResourceHandlerRegistry registry) {
         registry.addResourceHandler(PREFIX_STATIC_RESOURCES + "/" + version.getVersion() + "/**")
                 .addResourceLocations("classpath:/static/")
